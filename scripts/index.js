6fd3c8d43ee9a9e834b9ed24ff5a821d632cca6b
Sergey Baturin <toko-nn@yandex.ru>
2022-01-16 00:24:48 +0300

    open and close popup js

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,17 @@
-const sectionProfileButtonAdd = document.querySelector('.profile__button-add');
-console.log(sectionProfileButtonAdd);
+const profileOpenPopupButtonEdit = document.querySelector('.profile__button-edit')
+
+const popupEditProfileClose = document.querySelector('.popup-edit__close')
+
+const popupEditProfile = document.querySelector('.popup-edit')
+
+function OpenPopupEdit() {
+  popupEditProfile.classList.add('popup-edit_opened')
+}
+
+profileOpenPopupButtonEdit.addEventListener('click', OpenPopupEdit)
+
+function ClosePopupEdit() {
+  popupEditProfile.classList.remove('popup-edit_opened')
+}
+
+popupEditProfileClose.addEventListener('click', ClosePopupEdit)
