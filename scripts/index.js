2bc6ad96d06da1518b26876bb0daef33adc78235
Sergey Baturin <toko-nn@yandex.ru>
2022-01-16 16:17:45 +0300

    Подгружае данные в popup и выставляем ширину

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,23 @@
 const profileOpenPopupButtonEdit = document.querySelector('.profile__button-edit')
-
 const popupEditProfileClose = document.querySelector('.popup-edit__close')
-
 const popupEditProfile = document.querySelector('.popup-edit')
-
+const profileName = document.querySelector('.profile__name')
+const profileSubtitle = document.querySelector('.profile__subtitle')
+const popupEditName = document.querySelector('.popup-edit__name')
+const popupEditComment = document.querySelector('.popup-edit__comment')
+const elementsGroupButtonHeart = document.querySelectorAll('.elements__group')
 function openPopupEdit(event) {
   if (event.which === 1) {
     popupEditProfile.classList.add('popup-edit_opened')
+    popupEditName.value = profileName.textContent
+    popupEditComment.value = profileSubtitle.textContent
   }
 }
-
-profileOpenPopupButtonEdit.addEventListener('mousedown', openPopupEdit)
-
+elementsGroupButtonHeart.forEach(function(card) {
+  card.addEventListener('click', function(event) {
+    event.target.classList.toggle('elements__group_active')
+  })
+})
 function closePopupEdit(event) {
   if (event.which === 1) {
     if (event.currentTarget === event.target || popupEditProfileClose === event.currentTarget) {
@@ -20,19 +26,6 @@ function closePopupEdit(event) {
     }
   }
 }
-
 popupEditProfileClose.addEventListener('mousedown', closePopupEdit)
-
+profileOpenPopupButtonEdit.addEventListener('mousedown', openPopupEdit)
 popupEditProfile.addEventListener('mousedown', closePopupEdit)
-
-//================== Открытие и закрытие Popup Edit =============================
-
-const elementsGroupButtonHeart = document.querySelectorAll('.elements__group')
-
-elementsGroupButtonHeart.forEach(function(card) {
-  card.addEventListener('click', function(event) {
-    event.target.classList.toggle('elements__group_active')
-  })
-})
-
-//============== Смена цвета сердечка на карточке при нажатии ====================
