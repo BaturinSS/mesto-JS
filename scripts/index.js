9e8b7f20d829b16e0338ed43b02d638399c7b15d
Sergey Baturin <toko-nn@yandex.ru>
2022-01-16 17:10:40 +0300

    100%

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,8 @@ const profileSubtitle = document.querySelector('.profile__subtitle')
 const popupEditName = document.querySelector('.popup-edit__name')
 const popupEditComment = document.querySelector('.popup-edit__comment')
 const elementsGroupButtonHeart = document.querySelectorAll('.elements__group')
+const popupSaveButton = document.querySelector('.popup__save-button')
+const formPopupEdit = document.querySelector('.popup-edit__block')
 function openPopupEdit(event) {
   if (event.which === 1) {
     popupEditProfile.classList.add('popup-edit_opened')
@@ -22,10 +24,16 @@ function closePopupEdit(event) {
   if (event.which === 1) {
     if (event.currentTarget === event.target || popupEditProfileClose === event.currentTarget) {
       popupEditProfile.classList.remove('popup-edit_opened')
-      console.log(event)
     }
   }
+}
+function getFormValue(event) {
+  event.preventDefault()
+  profileName.textContent = popupEditName.value
+  profileSubtitle.textContent = popupEditComment.value
+  popupEditProfile.classList.remove('popup-edit_opened')
 }
 popupEditProfileClose.addEventListener('mousedown', closePopupEdit)
 profileOpenPopupButtonEdit.addEventListener('mousedown', openPopupEdit)
 popupEditProfile.addEventListener('mousedown', closePopupEdit)
+formPopupEdit.addEventListener('submit', getFormValue)
