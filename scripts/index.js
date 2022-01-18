73d13aa54be01eab2fc343fdce04d30fdc9e5b62
Sergey Baturin <toko-nn@yandex.ru>
2022-01-18 18:43:07 +0300

    Исправил ошибки после ревьюера18.01

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,6 @@ const profileName = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const popupEditName = document.querySelector('.popup-edit__name');
 const popupEditComment = document.querySelector('.popup-edit__comment');
-const elementsGroupButtonHeart = document.querySelectorAll('.elements__group');
-const popupSaveButton = document.querySelector('.popup__save-button');
 const formPopupEdit = document.querySelector('.popup-edit__block');
 function openPopupEdit(event) {
   if (event.which === 1) {
@@ -15,25 +13,15 @@ function openPopupEdit(event) {
     popupEditComment.value = profileSubtitle.textContent;
   }
 }
-elementsGroupButtonHeart.forEach(function(card) {
-  card.addEventListener('click', function(event) {
-    event.target.classList.toggle('elements__group_active');
-  })
-})
-function closePopupEdit(event) {
-  if (event.which === 1) {
-    if (event.currentTarget === event.target || popupEditProfileClose === event.currentTarget) {
-      popupEditProfile.classList.remove('popup-edit_opened');
-    }
-  }
+function closePopupEdit() {
+  popupEditProfile.classList.remove('popup-edit_opened');
 }
 function getFormValue(event) {
   event.preventDefault();
   profileName.textContent = popupEditName.value;
   profileSubtitle.textContent = popupEditComment.value;
-  popupEditProfile.classList.remove('popup-edit_opened');
+  closePopupEdit();
 }
-popupEditProfileClose.addEventListener('mousedown', closePopupEdit);
-profileOpenPopupButtonEdit.addEventListener('mousedown', openPopupEdit);
-popupEditProfile.addEventListener('mousedown', closePopupEdit);
+profileOpenPopupButtonEdit.addEventListener('click', openPopupEdit);
+popupEditProfileClose.addEventListener('click', closePopupEdit);
 formPopupEdit.addEventListener('submit', getFormValue);
