ffa7b19b6262973adca6e1367875c4c593413cd8
Sergey Baturin <toko-nn@yandex.ru>
2022-01-26 20:08:21 +0300

    Подключил js с информацией

--- scripts/index.js
+++ scripts/index.js
@@ -15,32 +15,6 @@ const elementsCards = document.querySelector('.elements__cards');
 const popupTitle = document.querySelector('.popup__title');
 const popupZoomSubtitle = document.querySelector('.popup-zoom__subtitle');
 const popupZoomImage = document.querySelector('.popup-zoom__image');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 initialCards.forEach((el) => creatureElementCard(el));
 function creatureElementCard(el) {
   const elementCard = elementsTemplate.querySelector('.elements__element').cloneNode(true);
