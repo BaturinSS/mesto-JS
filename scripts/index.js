d7621f11a6cbe04a9ead1b641f9ace1688324319
Sergey Baturin <toko-nn@yandex.ru>
2022-01-27 22:37:56 +0300

    Исправил все кроме функций

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,16 @@ const popupEditProfile = document.querySelector('.popup_edit-profile');
 const popupEditProfileClose = popupEditProfile.querySelector('.popup__close');
 const profileOpenPopupButtonEdit = document.querySelector('.profile__button-edit');
 const popupFormEdit = popupEditProfile.querySelector('.popup__form');
-const popupEditName = document.getElementsByName('name')[0];
-const popupEditAboutMe = document.getElementsByName('aboutMe')[0];
+const inputUserName = document.querySelector('.popup__input_user-name');
+const inputUserProfession = document.querySelector('.popup__input_user-profession');
 const profileName = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const popupAddCard = document.querySelector('.popup_add-card');
 const popupAddCardClose = popupAddCard.querySelector('.popup__close');
 const profileOpenPopupButtonAdd = document.querySelector('.profile__button-add');
 const popupFormAdd = popupAddCard.querySelector('.popup__form');
-const popupAddTitlePicture = document.getElementsByName('titlePicture')[0];
-const popupAddLinkPicture = document.getElementsByName('linkPicture')[0];
+const inputCardTitle = document.querySelector('.popup__input_card-title');
+const inputCardLink = document.querySelector('.popup__input_card-link');
 const popupZoom = document.querySelector('.popup_image-zoom');
 const popupZoomClose = popupZoom.querySelector('.popup__close');
 const popupZoomSubtitle = popupZoom.querySelector('.popup__subtitle');
@@ -20,6 +20,7 @@ const elementsTemplate = document.querySelector('#elements-template').content;
 const elementsCards = document.querySelector('.elements__cards');
 initialCards.forEach(el => createCard(el));
 function createCard(el) {
+  console.log(el);
   const elementCard = elementsTemplate.querySelector('.elements__element').cloneNode(true);
   elementCard.querySelector('.elements__title').textContent = el.name;
   elementCard.querySelector('.elements__delete').addEventListener('click', event => event.currentTarget.closest('.elements__element').remove());
@@ -34,30 +35,32 @@ function addCard(card) {elementsCards.prepend(card)};
 function openPopup(event) {
   if (event.target === profileOpenPopupButtonEdit) {
     popupEditProfile.classList.add('popup_opened');
-    popupEditName.value = profileName.textContent;
-    popupEditAboutMe.value = profileSubtitle.textContent;
+    inputUserName.value = profileName.textContent;
+    inputUserProfession.value = profileSubtitle.textContent;
   } else if (event.target === profileOpenPopupButtonAdd) {
     popupAddCard.classList.add('popup_opened');
   } else {
     popupZoom.classList.add('popup_opened');
     popupZoomImage.src = event.target.src;
+    popupZoomImage.alt = event.target.alt;
     const elementRectangle = event.target.closest('.elements__rectangle');
     popupZoomSubtitle.textContent = elementRectangle.querySelector('.elements__title').textContent;
   }
 }
 function closePopup(event) {
-  event.target.closest('.popup').classList.remove('popup_opened');
+  const popupActive = event.target.closest('.popup');
+  popupActive.classList.remove('popup_opened');
 }
 function getFormValue(event) {
   event.preventDefault();
   if (event.currentTarget === popupFormEdit) {
-    profileName.textContent = popupEditName.value;
-    profileSubtitle.textContent = popupEditAboutMe.value;
+    profileName.textContent = inputUserName.value;
+    profileSubtitle.textContent = inputUserProfession.value;
   } else if (event.currentTarget === popupFormAdd) {
-    const formData = [{name: popupAddTitlePicture.value, link: popupAddLinkPicture.value}]
+    const formData = [{name: inputCardTitle.value, link: inputCardLink.value}]
     formData.forEach(el => createCard(el));
-    popupAddTitlePicture.value = '';
-    popupAddLinkPicture.value = '';
+    inputCardTitle.value = '';
+    inputCardLink.value = '';
   }
   closePopup(event);
 }
