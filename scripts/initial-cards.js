6550029690928aa73655c141d927d971c0962c81
Sergey Baturin <toko-nn@yandex.ru>
2022-02-02 12:24:00 +0300

    test

--- scripts/initial-cards.js
+++ scripts/initial-cards.js
@@ -1,15 +1,15 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Кремль',
+    link: 'https://pbs.twimg.com/media/D6XK2mGXoAMZsAF.jpg:large'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Стадион',
+    link: 'https://pbs.twimg.com/media/DeRQa3UX0AAsH1-.jpg:large'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Чкаловская лестница',
+    link: 'https://sun6-21.userapi.com/impg/d_o0grcb3Dld2vBpLu5t6HvPegxCN4Zx_PsZCw/0n9EqMhnPgw.jpg?size=1280x914&quality=96&sign=61f893f021e56d6a1e4f1ebdf491b223&c_uniq_tag=Kd1C4sl2ef5hHAdB7IdPkla6JazlJAr6oxFsfLR1P2Q&type=album'
   },
   {
     name: 'Камчатка',
