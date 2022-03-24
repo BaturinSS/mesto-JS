fe9f7cf73347bd5fc804f0de562c56012a3c5b49
Sergey Baturin <toko-nn@yandex.ru>
2022-03-24 03:58:48 +0300

    instal Babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = {
   entry: { main: './src/scripts/index.js' },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
       publicPath: ''
   },
-  
+
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
@@ -15,4 +15,14 @@ module.exports = {
     port: 8080,
     open: true
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      }
+    ]
+  }
 }
