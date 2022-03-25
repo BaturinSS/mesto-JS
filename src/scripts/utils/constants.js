f6100c6b332b41c13316087bc0c498026e6c5886
Sergey Baturin <toko-nn@yandex.ru>
2022-03-25 22:38:57 +0300

    test

---
+++ src/scripts/utils/constants.js
@@ -0,0 +1,17 @@
+export const popupEditProfile = document.querySelector('.popup_type_profile-edit');
+
+export const profileOpenPopupButtonEdit = document.querySelector('.profile__button-edit');
+
+export const formEditProfile = popupEditProfile.querySelector('.popup__form');
+
+export const inputUserName = document.querySelector('.popup__input_user-name');
+
+export const inputUserProfession = document.querySelector('.popup__input_user-profession');
+
+export const popupAddCard = document.querySelector('.popup_type_card-add');
+
+export const profileOpenPopupButtonAdd = document.querySelector('.profile__button-add');
+
+export const formAddCard = popupAddCard.querySelector('.popup__form');
+
+export const selectorTemplate = '#elements-template';
