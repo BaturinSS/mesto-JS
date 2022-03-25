b7d9709185236cb1bc4929fb11f1ded38ea47c40
Sergey Baturin <toko-nn@yandex.ru>
2022-03-25 15:14:51 +0300

    made a request to the DOM in the constructor

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -4,7 +4,7 @@ export class Card {
     this._link = cardInfo.link;
     this._selectorTemplate = selectorTemplate;
     this._handleImageClick = handleImageClick;
-  }
+  };
 
   _getTemplate() {
     const elementCard = document
@@ -13,29 +13,31 @@ export class Card {
     .querySelector('.elements__element')
     .cloneNode(true);
     return elementCard;
-  }
+  };
 
   generateCard() {
     this._element = this._getTemplate();
     this._elementImage = this._element.querySelector('.elements__mask-group');
+    this._likeButton = this._element.querySelector('.elements__group');
     this._setEventListeners();
     this._element.querySelector('.elements__title').textContent = this._name;
     this._elementImage.src = this._link;
     this._elementImage.alt = this._name;
     return this._element;
-  }
+  };
 
   _setEventListeners() {
     this._element.querySelector('.elements__delete').addEventListener('click', () => {
       this._element.remove();
       this._element = null;
-    })
+    });
 
-    this._element.querySelector('.elements__group').addEventListener('click', (event) => {
-      event.target.classList.toggle('elements__group_active')});
+    this._likeButton.addEventListener('click', () => this._handleLikeButton());
 
-    this._elementImage.addEventListener('click', () => {
-      this._handleImageClick();
-    })
-  }
-}
+    this._elementImage.addEventListener('click', this._handleImageClick);
+  };
+
+  _handleLikeButton() {
+    this._likeButton.classList.toggle('elements__group_active');
+  };
+};
--- src/scripts/components/PopupWithForm.js
+++ src/scripts/components/PopupWithForm.js
@@ -4,27 +4,27 @@ export class PopupWithForm extends Popup {
   constructor(popupSelector, handleSabmit) {
     super(popupSelector);
     this._handleSabmit = handleSabmit;
-    this._form = this._popup.querySelector('.popup__form')
-  }
+    this._form = this._popup.querySelector('.popup__form');
+    this._inputs = [...this._form.querySelectorAll('.popup__input')];
+  };
 
   close() {
     super.close();
     this._form.reset();
-  }
+  };
 
   _getInputValues() {
-    const inputs = [...this._form.querySelectorAll('.popup__input')];
     const values = {};
-    inputs.forEach((input) => {
+    this._inputs.forEach((input) => {
       values[input.name] = input.value;
     });
     return values;
-  }
+  };
 
   setEventListeners() {
     super.setEventListeners();
     this._form.addEventListener('submit', () => {
       this._handleSabmit(this._getInputValues());
-    })
-  }
-}
+    });
+  };
+};
