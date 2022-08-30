b8d978cffde360d1b2cfea6ec34de2651f470623
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-08-30 21:07:27 +0300

    correct date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,63 @@
-import '../pages/index.css';
-
-import { Card } from '../scripts/components/Card';
-import { FormValidator } from '../scripts/components/FormValidator';
-import { Section } from '../scripts/components/Section';
-import { PopupWithImage } from '../scripts/components/PopupWithImage';
-import { PopupWithForm } from '../scripts/components/PopupWithForm';
-import { PopupWithConfirm } from '../scripts/components/PopupWithConfirm';
-import { UserInfo } from '../scripts/components/UserInfo';
-
-import { config } from '../scripts/utils/configValidation';
-import * as constants from '../scripts/utils/constants';
-import { api } from '../scripts/components/Api';
+import "../pages/index.css";
+import { Card } from "../scripts/components/Card";
+import { FormValidator } from "../scripts/components/FormValidator";
+import { Section } from "../scripts/components/Section";
+import { PopupWithImage } from "../scripts/components/PopupWithImage";
+import { PopupWithForm } from "../scripts/components/PopupWithForm";
+import { PopupWithConfirm } from "../scripts/components/PopupWithConfirm";
+import { UserInfo } from "../scripts/components/UserInfo";
+import { config } from "../scripts/utils/configValidation";
+import * as constants from "../scripts/utils/constants";
+import { api } from "../scripts/components/Api";
 
 const formAddCardValidator = new FormValidator(config, constants.formAddCard);
-const formEditProfileValidator = new FormValidator(config, constants.formEditProfile);
-const formEditAvatarValidator = new FormValidator(config, constants.formEditAvatar);
 
-const section = new Section(addCard, '.elements__cards');
+const formEditProfileValidator = new FormValidator(
+  config,
+  constants.formEditProfile
+);
 
-const popupWithImage = new PopupWithImage('.popup_type_image-zoom');
+const formEditAvatarValidator = new FormValidator(
+  config,
+  constants.formEditAvatar
+);
 
-const popupAddImage = new PopupWithForm('.popup_type_card-add', submitAddCardForm, 'Добавляем карточку...');
-const popupEditProfile = new PopupWithForm('.popup_type_profile-edit', submitEditProfileForm, 'Сохраняем...');
-const popupEditAvatar = new PopupWithForm('.popup_type_avatar-edit', submitEditAvatarForm, 'Меняем аватар...');
+const section = new Section(addCard, ".elements__cards");
 
-const popupDeleteCard = new PopupWithConfirm('.popup_type_delete-confirm', 'Удаляем карточку...');
+const popupWithImage = new PopupWithImage(".popup_type_image-zoom");
+
+const popupAddImage = new PopupWithForm(
+  ".popup_type_card-add",
+  submitAddCardForm,
+  "Добавляем карточку..."
+);
+const popupEditProfile = new PopupWithForm(
+  ".popup_type_profile-edit",
+  submitEditProfileForm,
+  "Сохраняем..."
+);
+const popupEditAvatar = new PopupWithForm(
+  ".popup_type_avatar-edit",
+  submitEditAvatarForm,
+  "Меняем аватар..."
+);
+
+const popupDeleteCard = new PopupWithConfirm(
+  ".popup_type_delete-confirm",
+  "Удаляем карточку..."
+);
 
 const userInfo = new UserInfo({
-  profileNameSelector: '.profile__name',
-  profileJobSelector: '.profile__subtitle',
-  avatarSelector: '.plofile__avatar'
+  profileNameSelector: ".profile__name",
+  profileJobSelector: ".profile__subtitle",
+  avatarSelector: ".plofile__avatar",
 });
 
 const autoDate = () => {
-  document.querySelector('.footer__copyright')
-    .textContent = `© 2022 - ${new Date().getFullYear()}. Батурин Сергей`;
-}
+  document.querySelector(
+    ".footer__copyright"
+  ).textContent = `© ${new Date().getFullYear()}. Батурин Сергей`;
+};
 
 let userId;
 
@@ -43,48 +65,45 @@ function openEditProfilePopup() {
   popupEditProfile.setInputValues(userInfo.getUserInfo());
   formEditProfileValidator.clearErrorsForm();
   popupEditProfile.open();
-};
+}
 
 function openAddImagePopup() {
   formAddCardValidator.clearErrorsForm();
   popupAddImage.open();
-};
+}
 
 function openEditAvatarPopup() {
   formEditAvatarValidator.clearErrorsForm();
   popupEditAvatar.open();
-};
+}
 
 function submitEditAvatarForm({ avatarUrl }) {
-  return api.editAvatar(avatarUrl)
-    .then((res) => {
-      userInfo.setUserInfo(res.name, res.about, res.avatar);
-    })
-};
+  return api.editAvatar(avatarUrl).then((res) => {
+    userInfo.setUserInfo(res.name, res.about, res.avatar);
+  });
+}
 
 function submitAddCardForm(data) {
   const createdSubmit = true;
-  return api.addCard(data.cardTitle, data.cardLink)
-    .then((res) => {
-      addCard(res, createdSubmit);
-      deleteLastCard();
-    })
-};
+  return api.addCard(data.cardTitle, data.cardLink).then((res) => {
+    addCard(res, createdSubmit);
+    deleteLastCard();
+  });
+}
 
 function submitEditProfileForm({ name, job }) {
-  return api.editUserInfo(name, job)
-    .then((res) => {
-      userInfo.setUserInfo(res.name, res.about, res.avatar);
-    })
-};
+  return api.editUserInfo(name, job).then((res) => {
+    userInfo.setUserInfo(res.name, res.about, res.avatar);
+  });
+}
 
 function deleteLastCard() {
-  document.querySelector('.elements__cards').lastElementChild.remove();
+  document.querySelector(".elements__cards").lastElementChild.remove();
 }
 
 function addCard(cardInfo, createdSubmit) {
   section.setItem(createCard(cardInfo), createdSubmit);
-};
+}
 
 function createCard(cardInfo) {
   const card = new Card(
@@ -96,56 +115,62 @@ function createCard(cardInfo) {
     (id) => {
       popupDeleteCard.open();
       popupDeleteCard.changeSubmitHandler(() => {
-        api.deleteCard(id)
+        api
+          .deleteCard(id)
           .then(() => {
             card.deleteCard();
             popupDeleteCard.close();
-            api.getCards()
-              .then(cardList => {
+            api
+              .getCards()
+              .then((cardList) => {
                 addCard(cardList[cardList.length - 1], false);
               })
               .catch((err) => {
                 err.then((res) => {
-                  alert(res.message)
-                })
-              })
+                  alert(res.message);
+                });
+              });
           })
           .catch((err) => {
             err.then((res) => {
-              alert(res.message)
-            })
+              alert(res.message);
+            });
           })
-          .finally(() => { popupDeleteCard.renderLoading(false); })
+          .finally(() => {
+            popupDeleteCard.renderLoading(false);
+          });
       });
     },
     userId,
     (id) => {
       if (card.isLiked()) {
-        api.deleteLike(id)
+        api
+          .deleteLike(id)
           .then((res) => {
             card.setLikes(res.likes);
           })
           .catch((err) => {
             err.then((res) => {
-              alert(res.message)
-            })
-          })
+              alert(res.message);
+            });
+          });
       } else {
-        api.addLike(id)
+        api
+          .addLike(id)
           .then((res) => {
             card.setLikes(res.likes);
           })
           .catch((err) => {
             err.then((res) => {
-              alert(res.message)
-            })
-          })
+              alert(res.message);
+            });
+          });
       }
     }
   );
   const cardElement = card.generateCard();
   return cardElement;
-};
+}
 
 Promise.all([api.getUserInfo(), api.getCards()])
   .then(([userData, cards]) => {
@@ -155,13 +180,22 @@ Promise.all([api.getUserInfo(), api.getCards()])
   })
   .catch((err) => {
     err.then((res) => {
-      alert(res.message)
-    })
-  })
+      alert(res.message);
+    });
+  });
 
-constants.profileOpenPopupButtonEdit.addEventListener('click', openEditProfilePopup);
-constants.profileOpenPopupButtonAdd.addEventListener('click', openAddImagePopup);
-constants.avatarOpenPopupButtonEdit.addEventListener('click', openEditAvatarPopup);
+constants.profileOpenPopupButtonEdit.addEventListener(
+  "click",
+  openEditProfilePopup
+);
+constants.profileOpenPopupButtonAdd.addEventListener(
+  "click",
+  openAddImagePopup
+);
+constants.avatarOpenPopupButtonEdit.addEventListener(
+  "click",
+  openEditAvatarPopup
+);
 
 formAddCardValidator.enableValidation();
 formEditProfileValidator.enableValidation();
